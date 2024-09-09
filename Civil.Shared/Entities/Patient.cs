9cca50386ca92bb008ef1836fabee6cd845439e7
MANUEL ANTONIO GUZMAN SANCHEZ <191478@iberopuebla.mx>
2024-09-09 01:33:16 -0600

    Asociacion civil

---
+++ Civil.Shared/Entities/Patient.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Civil.Shared.Entities
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Register { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string LastName { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string CURP { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string INE { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Result { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Phone { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string EmergencyPhone { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Date { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Gender { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Tipe { get; set; } = null!;
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres")]
+        public string Gravity { get; set; } = null!;
+
+    }
+}
---
+++ Civil.Web/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Civil.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Civil.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<Patient> Patient { get; set; }
+        
+        public DataContext(DbContextOptions<DataContext> options):base(options) 
+        { 
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Patient>().HasIndex(x => x.Register).IsUnique();
+        }
+
+
+
+    }
+}
